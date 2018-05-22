3da1b58f5515d3acf87268450817619057b9645e
Renzo <renzovh@kpnplanet.nl>
2018-05-22 11:59:08 +0200

    Alles
    jja

---
+++ Boodschappie/Boodschappie/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Boodschappie
+{
+    [Activity(Label = "Boodschappie", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+        Button button;
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            button = FindViewById<Button>(Resource.Id.btnMaaltijd);
+
+            button.Click += Button_Click;
+            //private void configureNextButton() {
+            //        Button nextButton = (Button)FindViewById(Resource.Id.btnMaaltijd);
+            //        nextButton.SetOnClickListener(new View.OnClickListener()
+            //            @Override
+            //            public void onClick(View view());   
+            //                StartActivity (new int)
+            //}
+
+
+        }
+
+
+void Button_Click(object sender, System.EventArgs e)
+        {
+            SetContentView(Resource.Layout.layout1);
+        }
+
+    }
+}
